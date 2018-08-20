38210c8734debc301fd0503ddca304cff8f61160
BalajiTE <balaji.telugunti@yahoo.com>
2018-08-20 05:58:26 -0400

    Test

--- WebApiFormsAuthentication/App_Start/WebApiConfig.cs
+++ WebApiFormsAuthentication/App_Start/WebApiConfig.cs
@@ -14,6 +14,8 @@ namespace WebApiFormsAuthentication
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
